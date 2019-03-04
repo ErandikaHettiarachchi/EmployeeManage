4b578458633283cd3599892dbf1c371c09d8d0b7
Erandika Hettiarachchi <ErandikaHettiarachchi@users.noreply.github.com>
2019-03-04 20:12:18 +0530

    deleted old

--- Employee/employeeComman/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        <b>Version</b> 1.0
-    </div>
-    <strong>Copyright &copy; UCSC | Group 09 .</strong> All rights reserved.powered by dkeranz
-</footer>
